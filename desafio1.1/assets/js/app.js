0912432a082990d6e430d5f986459962718d41fe
vvera <viverace@gmail.com>
2021-07-21 22:10:52 -0400

    operaciones

--- desafio1.1/assets/js/app.js
+++ desafio1.1/assets/js/app.js
@@ -11,4 +11,16 @@ function validacion() {
     }
     return true;
 }
-  
+
+var num1= prompt("ingrese primer numero");
+num1= parseInt(num1);
+var num2= prompt("ingrese segundo numero");
+num2= parseInt(num2);
+
+if (validacion(num1,num2)){
+    alert(`la suma es: ${num1+num2}`)
+    alert(`la resta es: ${num1-num2}`)
+    alert(`la multiplicacion es: ${num1*num2}`)
+    alert(`la division es: ${num1/num2}`)
+    alert(`el modulo es: ${num1%num2}`)
+}
